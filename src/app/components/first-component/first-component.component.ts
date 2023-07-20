9f299dc56500e823550ba45e80151e63495a93cf
Eduardo Pereira <53060876+Eduardo007-lang@users.noreply.github.com>
2023-07-20 17:06:39 -0300

    Test

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+
+  name: string = "Eduardo";
+  age: number = 23;
+  job: string = "programador";
+  hobbie = ["Correr", "Estudar", "Jogar"];
+  car = {
+    name: "Polo",
+    year: 2020
+  }
+
+
+
+  constructor(){}
+  
+  ngOnInit():void {
+    
+  }
+ 
+
+}
